5358916ef6144750cef263da6fe3fc9894d27f8d
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-14 14:58:24 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+
+
 
 df = pd.read_csv("hackathon/testdata2.csv", encoding="utf-8")
 
@@ -10,5 +14,8 @@ with st.sidebar:
 
 
 st.header("DNB-Hackathon: Exil-Monographien") 
+
+m = folium.Map(df, zoom_start=5)
+marker_cluster = MarkerCluster().add_to(m)
 
 st.dataframe(df)
