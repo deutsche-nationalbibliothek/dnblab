e21dc1f41d5292a360c61f4ef066d1272b658edc
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-14 12:56:34 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
+
+with st.sidebar:
+  st.write("Test test test")
+  st.info("Diese App entstand im ersten Hackathon der DNB.")
 
 
 st.header("DNB-Hackathon: Exil-Monographien") 
