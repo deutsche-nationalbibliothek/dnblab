cac21dede875dfe90eadf624a2ff737c5dc076de
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-15 11:31:19 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -40,23 +40,4 @@ df_map = df.rename(columns={'long': 'lon'})
 st.map(df_map)
 
 
-st.subheader("Dritte Möglichkeit") 
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=lat,
-        longitude=long,
-        zoom=3,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df_map,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-      ],
-    ))
