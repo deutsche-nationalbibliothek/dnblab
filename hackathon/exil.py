e31962221db1a3a9d903f4dd4b65e636119e2b1a
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-15 08:58:59 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import folium
-#from folium.plugins import MarkerCluster
+import folium
+from folium.plugins import MarkerCluster
 import streamlit_folium
 
 
