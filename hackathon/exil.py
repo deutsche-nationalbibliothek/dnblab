5ae1abbad1f5770c2afb985a6204513e2a97ae70
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-15 09:21:05 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -17,12 +17,12 @@ with st.sidebar:
 
 st.header("DNB-Hackathon: Exil-Monographien") 
 
-#m = folium.Map(df, x="long", y="lat", zoom_start=5)
+
 #marker_cluster = MarkerCluster().add_to(m)
 
 st.dataframe(df)
 
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+m = folium.Map(df, x="long", y="lat", zoom_start=5)
+#m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
 marker_cluster = MarkerCluster().add_to(m)
 folium_static(m)
-#st.map(m)
