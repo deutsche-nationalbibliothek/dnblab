88b4692f4e498d009af38f228f5d98d230fad0c8
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-15 09:12:01 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -22,5 +22,5 @@ m = folium.Map(df, x="long", y="lat", zoom_start=5)
 st.dataframe(df)
 
 #m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
-#folium_static(m)
-st.map(m)
+folium_static(m)
+#st.map(m)
