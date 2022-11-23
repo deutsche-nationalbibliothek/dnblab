e14e9c513e646dd0989cd95a9cf5624b873a57df
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-11-23 07:58:40 +0100

    Update exil.py

--- hackathon/exil.py
+++ hackathon/exil.py
@@ -75,11 +75,15 @@ st.dataframe(df_query)
 # -- KARTE2
 st.markdown("#### Darstellung aller Exil-Monographien im Set nach Häufigkeit") 
 
-
 df_map = df.rename(columns={'long': 'lon'})
+df_map["Erscheinungsort"] = df_map["Erscheinungsort"].str.strip("[]")
+new = df_map.groupby(["Erscheinungsort"]).size().reset_index(name='counts')
+dfmerge = pd.merge(df_map, new, on=['Erscheinungsort'], how="left")
+places = dfmerge.drop_duplicates(['Erscheinungsort'], keep='first')
+places
 
-df_query2 = df_map
-st.map(df_query2)
+#df_query2 = df_map
+st.map(places)
 
 
 
